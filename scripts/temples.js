3310e36a5a96aec1c6202e7f7ec17e58e6a08cfa
Simphiwe Nkabinde <nka22001@byui.edu>
2024-09-28 03:28:36 +0200

    corrections

--- scripts/temples.js
+++ scripts/temples.js
@@ -3,5 +3,7 @@ const navigation = document.querySelector("#header-navigation");
 
 menuButton.addEventListener("click", () => {
     menuButton.classList.toggle("open");
+    const expanded = menuButton.getAttribute('aria-expanded') === 'true' || false;
+    menuButton.setAttribute('aria-expanded', !expanded);
     navigation.classList.toggle("open");
 })
